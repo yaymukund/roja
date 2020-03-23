8fbb666ff4ef61e17fffb92d00383bf1e96dbd9c
Mukund Lakshman <yaymukund@gmail.com>
2020-03-23 18:23:56 +0000

    Add some padding to the indicator

--- src/ui/player.rs
+++ src/ui/player.rs
@@ -1,6 +1,6 @@
 use crate::ui::{Label, LabelSet};
 use cursive::view::ViewWrapper;
-use cursive::views::{LinearLayout, TextView};
+use cursive::views::{LinearLayout, PaddedView, TextView};
 use cursive::wrap_impl;
 
 pub struct PlayerView {
@@ -13,8 +13,10 @@ impl PlayerView {
         let duration = label_set.get(&Label::TotalTime).clone();
         let play_pause = label_set.get(&Label::PlayPauseIndicator).clone();
 
+        let indicator = PaddedView::lrtb(0, 1, 0, 0, TextView::new_with_content(play_pause));
+
         let view = LinearLayout::horizontal()
-            .child(TextView::new_with_content(play_pause))
+            .child(indicator)
             .child(TextView::new_with_content(elapsed))
             .child(TextView::new("/"))
             .child(TextView::new_with_content(duration));
